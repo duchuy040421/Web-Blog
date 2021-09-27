884b18e0c0ffeda25cc6eb876fa7a56f10b043ad
duchuy040421 <57801256+duchuy040421@users.noreply.github.com>
2021-09-27 22:51:17 +0700

    complete-Home-page

--- js/function.js
+++ js/function.js
@@ -2,3 +2,19 @@ $(window).load(function() {
 	$('#loader').delay(1900).fadeOut('slow');
 });
 
+document.getElementById('button-element').addEventListener("click",button);
+
+var active = false;
+
+function button(){
+	if (active === false){
+		document.getElementById('menu-button').style.display = "flex";
+		document.getElementById('button-element').style.opacity = "0.7";
+	}
+	else{
+		document.getElementById('menu-button').style.display = "none";
+		document.getElementById('button-element').style.opacity = "1";
+	}
+
+	active = !active;
+}
